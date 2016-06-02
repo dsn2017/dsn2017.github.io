ec581ec16ab2cafc53c503935274875d95522e0d
dsn2017 <highlander.rahat@gmail.com>
2016-06-01 21:16:38 -0600

    Create title2.js

---
+++ includes/title2.js
@@ -0,0 +1,18 @@
+document.write("<table style=\"width:100%\">");
+document.write("<tr>");
+document.write("<td >	");
+document.write("<img src=\"../figures/DSN_2017.jpg\" width=\"100%\"/>");
+document.write("</td>");
+
+
+document.write("<td align=\"center\">");
+document.write("<h1>DSN 2017</h1>");
+document.write("<h2>The 47th IEEE/IFIP International Conference on Dependable Systems and Networks</h2>");
+document.write("<h3>June26-29,2017,Denver,CO,USA</h3>");
+	
+document.write("</td>");
+document.write("<td>");
+	
+document.write("</td>");
+document.write("</tr>");
+document.write("</table>");
