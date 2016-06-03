da4e2583e5ee8dd63829717e1ab6be22f43d7b9d
Rahat Ibn Rafiq <rahat.rafiq@colorado.edu>
2016-06-02 19:44:55 -0600

    commit

--- includes/title1.js
+++ includes/title1.js
@@ -8,7 +8,7 @@ document.write("</td>");
 document.write("<td align=\"center\">");
 document.write("<h1>DSN 2017</h1>");
 document.write("<h3>The 47th IEEE/IFIP International Conference on Dependable Systems and Networks</h3>");
-document.write("<h3>June 26-29,2017</h3>");
+document.write("<h3>June 26-29, 2017</h3>");
 document.write("<h3>Denver,CO,USA</h3>");
 	
 document.write("</td>");
